18fbb4ce8c775e373aeb2faadc1870cbc3339e2c
Franciszek Juras <franciszekjuras@gmail.com>
2019-03-27 19:05:25 +0100

    Added more src debugging.

--- fir.c
+++ fir.c
@@ -57,27 +57,37 @@ int zero_coefs(fir_t fir){
 }
 
 int load_coefs_dw(fir_t fir, coef_t* coefs){
+    FILE* dbgWrF = fopen("/tmp/dbgwrdw.txt", "w");
+
     fprintf(stderr, "Writing %d downsampling coefs to FPGA... ", fir.conf->src_coefs_nr);
         int k = 0; int tm = fir.conf->tm; int bs = fir.conf->base_shift;
         int dsp_nr = fir.conf->src_dsp_nr;
         coef_t mult = (coef_t)(1<<(fir.conf->src_coef_mag));
         for(int j = 0; j < dsp_nr; ++j)
-            for(int i = tm-1; i >= 0; --i)
+            for(int i = tm-1; i >= 0; --i){
                 fir.dwsamp_coefs[(j<<bs)+i] = (int32_t)round(mult*coefs[k++]);
+                fprintf(dbgWrF, "%o %d %o %d\n", (j<<bs)+i, (int32_t)round(mult*coefs[k-1]), fir.conf->dbg_wr_addr, fir.conf->dbg_wr_data);
+            }
         
         fprintf(stderr, "Done.\n");
+    fclose(dbgWrF);
     return 0;
 }
 int load_coefs_up(fir_t fir, coef_t* coefs){
+    FILE* dbgWrF = fopen("/tmp/dbgwrup.txt", "w");
+
     fprintf(stderr, "Writing %d upsampling coefs to FPGA... ", fir.conf->src_coefs_nr);
         int k = 0; int tm = fir.conf->tm; int bs = fir.conf->base_shift;
         int dsp_nr = fir.conf->src_dsp_nr;
         coef_t mult = (coef_t)((1<<(fir.conf->src_coef_mag)) * fir.conf->tm);
         for(int j = 0; j < dsp_nr; ++j)
-            for(int i = 0; i < tm; ++i)
+            for(int i = 0; i < tm; ++i){
                 fir.upsamp_coefs[(j<<bs)+i] = (int32_t)round(mult*coefs[k++]);
+                fprintf(dbgWrF, "%o %d %o %d\n", (j<<bs)+i, (int32_t)round(mult*coefs[k-1]), fir.conf->dbg_wr_addr, fir.conf->dbg_wr_data);
+            }
 
         fprintf(stderr, "Done.\n");
+    fclose(dbgWrF);
     return 0;
 }
 int load_coefs_fir(fir_t fir, coef_t* coefs){
